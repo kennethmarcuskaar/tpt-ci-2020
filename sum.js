61b78d2516834d6357ec161086b1cac4b68abcab
kennethmarcuskaar <58027431+kennethmarcuskaar@users.noreply.github.com>
2020-03-06 13:20:57 +0200

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,5 +1,5 @@
 function randonthindgmidagi(a = 0, b = 'kala') {
-  const a1 = 
+  const a1 = Number(a);
   const b1 = Number(b);
   if (Number.isNaN(a1) || Number.isNaN(b1)) {
     throw new Error('Invalid arguments');
